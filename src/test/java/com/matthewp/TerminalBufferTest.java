994973068425ad791047b38041c87ed2d715dc4e
Matthew Pappas <matteopappas@gmail.com>
2026-02-14 14:48:28 +0100

    test: add verification for clear and fillLine

--- src/test/java/com/matthewp/TerminalBufferTest.java
+++ src/test/java/com/matthewp/TerminalBufferTest.java
@@ -29,4 +29,24 @@ class TerminalBufferTest {
         assertEquals(3, buffer.getCursorX());
         assertEquals(1, buffer.getCursorY());
     }
+
+    @Test
+    void testClear() {
+        TerminalBuffer buffer = new TerminalBuffer(10, 2, 0);
+        buffer.write("Hoi");
+        buffer.setCursor(5, 1);
+
+        buffer.clear();
+
+        assertEquals(0, buffer.getCursorX());
+        assertEquals(0, buffer.getCursorY());
+        assertEquals("          ", buffer.getLineText(0));
+    }
+
+    @Test
+    void testFillLine() {
+        TerminalBuffer buffer = new TerminalBuffer(5, 1, 0);
+        buffer.fillLine(0, 'x');
+        assertEquals("xxxxx", buffer.getLineText(0));
+    }
 }
